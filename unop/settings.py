4b7e8c1df2948835349f173d7a9054e92c939a18
Juan Carlos <jcbianchi86@gmail.com>
2025-06-10 16:50:29 -0300

    Configuraciones Setting 2

--- unop/settings.py
+++ unop/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-8q0nb#-t!fr&2i1xx@1l0jsv68$$+(yk^7s$kha%4sb)k8o-81
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'unop-django.onrender.com']
 
 # Application definition
 
--- unop/wsgi.py
+++ unop/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for unop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unop.settings')
+
+import django
+django.setup()
+
+from django.core.management import call_command
+
+# Ejecutar migraciones automáticamente
+call_command('migrate', interactive=False)
+
+# Recopilar archivos estáticos
+call_command('collectstatic', interactive=False, clear=True)
 
 application = get_wsgi_application()
